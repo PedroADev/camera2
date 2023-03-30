5d56ee0c9a3e00ad2425529099c0367be5f97063
Pedro Mattos <pepoguto@gmail.com>
2023-03-30 15:59:12 -0300

    -comentarios2-

--- app/src/main/java/com/example/camera2/hello.java
+++ app/src/main/java/com/example/camera2/hello.java
@@ -10,14 +10,17 @@ import java.util.TimerTask;
 
 public class hello extends AppCompatActivity {
 
+    //criação de uma instancia da classe Timer
     private final Timer timer = new Timer();
     TimerTask timerTask;
 
+    //define o uso do layout activity_hello.xml
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
 
+        //executará a main activity quando o tempo limite for atingido
         timerTask = new TimerTask() {
             @Override
             public void run() {
@@ -35,6 +38,8 @@ public class hello extends AppCompatActivity {
 
 
         };
+
+        //adiciona um delay de 3s para a execução da tarefa
         timer.schedule(timerTask, 3000);
 
 
@@ -42,6 +47,7 @@ public class hello extends AppCompatActivity {
 
     }
 
+    //passado os 3s, inicia a main activity
     private void gotoMainActivity() {
 
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
